1ed6cf636af2ce9905f7ca11df5ac6bd1cdbf09a
dattatreya97 <dattu4621@gmail.com>
2018-10-04 00:04:16 +0530

    added user class

---
+++ question_bank/question_bank/user.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace question_bank
+{
+    public class user
+    {
+        string name;
+        string faculty_id;
+        string subject;
+        bool isAdmin;
+        bool isCoordinator;
+
+        public user(string faculty_id, string name,string subject,bool isAdmin,bool isCoordinator)
+        {
+            this.name = name;
+            this.faculty_id = faculty_id;
+            this.subject = subject;
+            this.isAdmin = isAdmin;
+            this.isCoordinator = isCoordinator;
+
+        }
+        
+    }
+}
