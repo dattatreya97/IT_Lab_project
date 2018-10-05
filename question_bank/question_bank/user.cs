7b94f0db1f39bed31f6f94d2f0883a70fa62de74
dattatreya97 <dattu4621@gmail.com>
2018-10-05 12:06:01 +0530

    login and admin functionality

--- question_bank/question_bank/user.cs
+++ question_bank/question_bank/user.cs
@@ -10,17 +10,22 @@ namespace question_bank
         string name;
         string faculty_id;
         string subject;
-        bool isAdmin;
-        bool isCoordinator;
+        int isAdmin;
+        int isCoordinator;
+        string semester;
+        string year;
+        string branch;
 
-        public user(string faculty_id, string name,string subject,bool isAdmin,bool isCoordinator)
+        public user(string faculty_id, string name,string subject,int isAdmin,int isCoordinator,string branch,string semester,string year)
         {
             this.name = name;
             this.faculty_id = faculty_id;
             this.subject = subject;
             this.isAdmin = isAdmin;
             this.isCoordinator = isCoordinator;
-
+            this.branch = branch;
+            this.semester = semester;
+            this.year = year;
         }
         
     }
